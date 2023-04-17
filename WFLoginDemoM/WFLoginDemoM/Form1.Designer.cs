b2cd452c7b02d382af02bff0022522db7571beb6
brixton3 <brixton3@hotmail.com>
2023-04-16 21:51:06 -0300

    picture box con gradiente personalizado y fondo con gradiente

--- WFLoginDemoM/WFLoginDemoM/Form1.Designer.cs
+++ WFLoginDemoM/WFLoginDemoM/Form1.Designer.cs
@@ -28,13 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.circularPictureBox1 = new WFLoginDemoM.Forms.CircularPictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.circularPictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // circularPictureBox1
+            // 
+            this.circularPictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.circularPictureBox1.BorderCapStyle = System.Drawing.Drawing2D.DashCap.Flat;
+            this.circularPictureBox1.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.circularPictureBox1.BorderColor2 = System.Drawing.Color.HotPink;
+            this.circularPictureBox1.BorderLineStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            this.circularPictureBox1.BorderSize = 5;
+            this.circularPictureBox1.GradientAngle = 50F;
+            this.circularPictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("circularPictureBox1.Image")));
+            this.circularPictureBox1.Location = new System.Drawing.Point(285, 137);
+            this.circularPictureBox1.Name = "circularPictureBox1";
+            this.circularPictureBox1.Size = new System.Drawing.Size(224, 219);
+            this.circularPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.circularPictureBox1.TabIndex = 0;
+            this.circularPictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MediumPurple;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 93);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("JetBrains Mono", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(160, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(454, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "picturebox gradiente personalizado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.circularPictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.circularPictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Forms.CircularPictureBox circularPictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
---
+++ WFLoginDemoM/WFLoginDemoM/Forms/CircularPictureBox.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.ComponentModel;
+
+namespace WFLoginDemoM.Forms
+{
+    class CircularPictureBox : PictureBox
+    {
+        //fields
+        private int borderSize = 2;
+        private Color borderColor = Color.RoyalBlue;
+        private Color borderColor2 = Color.HotPink;
+        private DashStyle borderLineStyle = DashStyle.Solid;
+        private DashCap borderCapStyle = DashCap.Flat;
+        private float gradientAngle = 50F;
+
+        //constructor
+
+        public CircularPictureBox()
+        {
+            this.Size = new Size(100, 100);
+            this.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        public int BorderSize
+        {
+            get
+            {
+                return borderSize;
+            }
+
+            set
+            {
+                borderSize = value;
+                this.Invalidate();
+            }
+        }
+
+        public Color BorderColor
+        {
+            get
+            {
+                return borderColor;
+            }
+
+            set
+            {
+                borderColor = value;
+                this.Invalidate();
+            }
+        }
+
+        public Color BorderColor2
+        {
+            get
+            {
+                return borderColor2;
+                this.Invalidate();
+            }
+
+            set
+            {
+                borderColor2 = value;
+            }
+        }
+
+        public DashStyle BorderLineStyle
+        {
+            get
+            {
+                return borderLineStyle;
+            }
+
+            set
+            {
+                borderLineStyle = value;
+                this.Invalidate();
+            }
+        }
+
+        public DashCap BorderCapStyle
+        {
+            get
+            {
+                return borderCapStyle;
+            }
+
+            set
+            {
+                borderCapStyle = value;
+                this.Invalidate();
+            }
+        }
+
+        public float GradientAngle
+        {
+            get
+            {
+                return gradientAngle;
+            }
+
+            set
+            {
+                gradientAngle = value;
+                this.Invalidate();
+            }
+        }
+
+        //overridden methods
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            this.Size = new Size(this.Width, this.Height);
+        }
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+            //fields
+            var graph = pe.Graphics;
+            var rectContourSmooth = Rectangle.Inflate(this.ClientRectangle, -1, -1);
+            var rectBorder = Rectangle.Inflate(rectContourSmooth, -borderSize, -borderSize);
+            var smoothSize = borderSize > 0 ? borderSize * 3 : 1;
+            using (var borderGColor = new LinearGradientBrush(rectBorder, borderColor, BorderColor2, gradientAngle))
+            using (var pathRegion = new GraphicsPath())
+            using (var penSmooth = new Pen(this.Parent.BackColor, smoothSize))
+            using (var penBorder = new Pen(borderGColor, BorderSize))
+            {
+                penBorder.DashStyle = BorderLineStyle;
+                penBorder.DashCap = BorderCapStyle;
+                pathRegion.AddEllipse(rectContourSmooth);
+                this.Region = new Region(pathRegion);
+                graph.SmoothingMode = SmoothingMode.AntiAlias;
+
+                //drawing
+                graph.DrawEllipse(penSmooth, rectContourSmooth);
+                if (BorderSize > 0)
+                    graph.DrawEllipse(penBorder, rectBorder);
+            }
+        }
+    }
+}
